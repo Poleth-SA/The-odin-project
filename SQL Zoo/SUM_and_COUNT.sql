897df2de5c75b2b6aef3089b23a802e93c6a0946
PD232001 <pdsalmeronacosta760@csu.fullerton.edu>
2024-08-28 13:44:15 -0700

    started section 6

---
+++ SQL Zoo/SUM_and_COUNT.sql
@@ -0,0 +1,9 @@
+/* the data given to use:
+name	continent	area 	population	gdp
+
+This tutorial is about aggregate functions such as COUNT, SUM and AVG. An aggregate function takes many values and delivers just one value. For example the function SUM would aggregate the values 2, 4 and 5 to deliver the single value 11.*/
+
+/* Question 1
+Show the total population of the world.
+
+world(name, continent, area, population, gdp)*/
